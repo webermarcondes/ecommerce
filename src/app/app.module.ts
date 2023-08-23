6b89cc4658627853bbebdd8346c55439534e95e0
Weber Marcondes <webermarcondes7@gmail.com>
2023-08-22 21:44:53 -0300

    configurando o firebase pro projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,11 @@ import { CategoriaComponent } from './categoria/categoria.component';
 import { CategoriaListarComponent } from './categoria/categoria-listar/categoria-listar.component';
 import { CategoriaFormularioComponent } from './categoria/categoria-formulario/categoria-formulario.component';
 
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,7 +29,19 @@ import { CategoriaFormularioComponent } from './categoria/categoria-formulario/c
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp({
+
+      apiKey: "AIzaSyByTC8xMAC9N8lXOF_qDKM4kbw42GwR5HU",
+      authDomain: "ecommerce-ads2023-4-2-c3a67.firebaseapp.com",
+      projectId: "ecommerce-ads2023-4-2-c3a67",
+      storageBucket: "ecommerce-ads2023-4-2-c3a67.appspot.com",
+      messagingSenderId: "105150751379",
+      appId: "1:105150751379:web:d802cac2dcb6edcbcbe366",
+      measurementId: "G-XZGZH53BN1"
+    }
+    ),
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
